51ff4f7a587ab5653efb7cb5c25cc68100db4f5a
Tobias Freese <wolfshund98@gmail.com>
2019-08-08 15:37:16 +0200

    Made the laser into a proper multiblock

--- src/main/java/technologicalmayhem/firstmod/block/BlockLaserBase.java
+++ src/main/java/technologicalmayhem/firstmod/block/BlockLaserBase.java
@@ -2,13 +2,17 @@ package technologicalmayhem.firstmod.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
@@ -18,13 +22,35 @@ import technologicalmayhem.firstmod.ModBlocks;
 
 public class BlockLaserBase extends Block {
 
+    public static final PropertyBool MIDDLE = PropertyBool.create("middle");
+    protected static final AxisAlignedBB LASER_BASE_MIDDLE_AABB = new AxisAlignedBB(0.25D, 0D, 0.25D, 0.75D, 1.0D, 0.75D);
+
     public BlockLaserBase() {
         super(Material.ROCK);
         setUnlocalizedName(FirstMod.MODID + ".laserbase");
         setRegistryName("laserbase");
         setHardness(2.0f);
         setCreativeTab(CreativeTabs.DECORATIONS);
-        setDefaultState(getDefaultState());
+        setDefaultState(getDefaultState().withProperty(MIDDLE, false));
+    }
+
+    @Override
+    public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
+        worldIn.setBlockState(pos.up(1), state.withProperty(MIDDLE, true));
+        worldIn.setBlockState(pos.up(2), ModBlocks.laserTurret.getDefaultState());
+    }
+
+    @Override
+    public void onBlockDestroyedByPlayer(World worldIn, BlockPos pos, IBlockState state) {
+        if (state.getValue(MIDDLE)) {
+            if (worldIn.getBlockState(pos.down()).getBlock() instanceof BlockLaserBase)
+                worldIn.setBlockToAir(pos.down());
+            if (worldIn.getBlockState(pos.up()).getBlock() instanceof BlockLaserTurret) worldIn.setBlockToAir(pos.up());
+        } else {
+            if (worldIn.getBlockState(pos.up(1)).getBlock() instanceof BlockLaserBase) worldIn.setBlockToAir(pos.up(1));
+            if (worldIn.getBlockState(pos.up(2)).getBlock() instanceof BlockLaserTurret)
+                worldIn.setBlockToAir(pos.up(2));
+        }
     }
 
     @SideOnly(Side.CLIENT)
@@ -34,8 +60,36 @@ public class BlockLaserBase extends Block {
     }
 
     @Override
-    public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
-        worldIn.setBlockState(pos.up(1), state);
-        worldIn.setBlockState(pos.up(2), ModBlocks.laserTurret.getDefaultState());
+    public int getMetaFromState(IBlockState state) {
+        return state.getValue(MIDDLE) ? 1 : 0;
+    }
+
+    @SuppressWarnings("NullableProblems")
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        return meta == 1 ? getDefaultState().withProperty(MIDDLE, true) : getDefaultState();
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, MIDDLE);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean isOpaqueCube(IBlockState state) {
+        return !state.getValue(MIDDLE);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean isFullCube(IBlockState state) {
+        return !state.getValue(MIDDLE);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+        return state.getValue(MIDDLE) ? LASER_BASE_MIDDLE_AABB : FULL_BLOCK_AABB;
     }
 }
--- src/main/java/technologicalmayhem/firstmod/block/BlockLaserTurret.java
+++ src/main/java/technologicalmayhem/firstmod/block/BlockLaserTurret.java
@@ -6,17 +6,25 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import technologicalmayhem.firstmod.FirstMod;
+import technologicalmayhem.firstmod.ModBlocks;
 import technologicalmayhem.firstmod.block.tile.TileLaserTurret;
 
 import javax.annotation.Nullable;
+import java.util.Random;
 
 public class BlockLaserTurret extends Block {
+
+    protected static final AxisAlignedBB LASER_TURRET_AABB = new AxisAlignedBB(0.25D, 0D, 0.25D, 0.75D, 0.75, 0.75D);
 
     public BlockLaserTurret() {
         super(Material.ROCK);
@@ -27,10 +35,21 @@ public class BlockLaserTurret extends Block {
         setDefaultState(getDefaultState());
     }
 
+    @Override
+    public void onBlockDestroyedByPlayer(World worldIn, BlockPos pos, IBlockState state) {
+        if (worldIn.getBlockState(pos.down(1)).getBlock() instanceof BlockLaserBase) worldIn.setBlockToAir(pos.down(1));
+        if (worldIn.getBlockState(pos.down(2)).getBlock() instanceof BlockLaserBase) worldIn.setBlockToAir(pos.down(2));
+    }
+
     @SideOnly(Side.CLIENT)
     public void initModel() {
         ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0,
                 new ModelResourceLocation(getRegistryName(), "inventory"));
+    }
+
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        return ItemBlock.getItemFromBlock(ModBlocks.laserBase);
     }
 
     @Override
@@ -42,5 +61,20 @@ public class BlockLaserTurret extends Block {
     @Override
     public TileEntity createTileEntity(World world, IBlockState state) {
         return new TileLaserTurret();
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+        return LASER_TURRET_AABB;
     }
 }
