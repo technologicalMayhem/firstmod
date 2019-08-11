bdd13d45f176fcbeeeba3dabb01f59d91bd2d604
Tobias Freese <wolfshund98@gmail.com>
2019-08-11 21:11:34 +0200

    Set custom model for laserbase in inventory

--- src/main/java/technologicalmayhem/firstmod/proxy/ClientProxy.java
+++ src/main/java/technologicalmayhem/firstmod/proxy/ClientProxy.java
@@ -1,6 +1,9 @@
 package technologicalmayhem.firstmod.proxy;
 
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.ItemBlock;
 import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
@@ -13,5 +16,7 @@ public class ClientProxy extends CommonProxy {
     public static void registerModels(ModelRegistryEvent event) {
         ModBlocks.initModels();
         ModItems.initModels();
+
+        ModelLoader.setCustomModelResourceLocation(new ItemBlock(ModBlocks.laserBase), 0, new ModelResourceLocation(ModBlocks.laserBase.getRegistryName(), "inventory"));
     }
 }
